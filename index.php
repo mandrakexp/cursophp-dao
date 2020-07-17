381bde25a5e4cbae2437dbc186afe621c55d2c69
mandrakexp <edgilson.suporte@gmail.com>
2020-07-17 11:48:09 -0300

    criamos adcionamos um list e search na classe usuario

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php 
 
 require_once("config.php");
-
  
+// carrega somente um usuario
+//$root = new Usuario();
 
-  $root = new Usuario();
+//$root->loadbyId(3);
 
- 
+//echo $root;
 
-  $root->loadbyId(3);
+//carrega uma lista de usuario 
 
-  echo $root;
+//$lista = Usuario::getlist();
+
+//echo json_encode($lista)
+
+// carrega uma lista buscando pelo login
+
+//$search = Usuario::search("e");
+
+//echo json_encode($search);
+
+// clientes conectados
+
+$usuario = new Usuario();
+$usuario->login("mandrakexp","batata");
+
+echo $usuario;
 
   
    ?>
